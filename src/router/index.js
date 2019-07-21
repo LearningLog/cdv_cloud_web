d158469695d9bc89ef901a57410620e7b70929b1
YanHuaKang <1060553506@qq.com>
2019-07-21 18:27:36 +0800

    msg

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import live from '@/views/live'
+import playback from '@/views/playback'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/live',
+      name: 'live',
+      component: live
+    },
+    {
+      path: '/playback',
+      name: 'playback',
+      component: playback
     }
   ]
 })
